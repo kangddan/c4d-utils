5747ec504f3132900da075a3ac4f26eb953eeff7
Kangddan <125175728+kangddan@users.noreply.github.com>
2024-08-03 18:47:18 +0800

    边和面模式下正确识别点ID生成定位器

--- myC4dScript/selectedPointToNull.py
+++ myC4dScript/selectedPointToNull.py
@@ -1,50 +1,94 @@
 import c4d
 
+class GetSelectedPointID(object):
 
-"""
-def getSelectedPoints(target):
-    bs = target.GetPointS()
-    return [i for i in range(target.GetPointCount()) if bs.IsSelected(i)]
-"""
+    def __init__(self, obj, modeId):
+        self.obj    = obj
+        self.modeId = modeId
 
-def getSelectedPoints(obj):
-    bs  = obj.GetPointS()
-    sel = bs.GetAll(obj.GetPointCount())
-    return [index for index, selected in enumerate(sel) if selected]
+    def run(self) -> list[int]:
+        if self.modeId == c4d.Mpoints:
+            return self.pointsToId(self.obj)
+        elif self.modeId == c4d.Medges:
+            return self.edgesToId(self.obj)
+        elif self.modeId == c4d.Mpolygons:
+            return self.polysToId(self.obj)
+        return []
 
+    @staticmethod
+    def pointsToId(obj) -> list[int]:
+        bs  = obj.GetPointS()
+        sel = bs.GetAll(obj.GetPointCount())
+        return [index for index, selected in enumerate(sel) if selected]
 
-def create(pos, name):
-    nullObj = c4d.BaseObject(c4d.Onull)
-    nullObj.SetName('{}_LOC_{}'.format(name[0], name[1]))
-    doc.AddUndo(c4d.UNDOTYPE_NEW, nullObj)
-    doc.InsertObject(nullObj)
+    @staticmethod
+    def polysToId(obj) -> set[int]:
+        bs  = obj.GetPolygonS()
+        sel = bs.GetAll(obj.GetPolygonCount())
+        selPolyId = [index for index, selected in enumerate(sel) if selected]
 
-    nullObj.SetMg(c4d.Matrix(off=pos))
-    nullObj[c4d.NULLOBJECT_DISPLAY] = 1
-    nullObj[c4d.NULLOBJECT_ORIENTATION] = 1
-    doc.SetSelection(nullObj, c4d.SELECTION_ADD)
+        # ---------------------------------------------------
+        polys: list[c4d.CPolygon] = obj.GetAllPolygons()
+        selPointsId = set()
+        for index in selPolyId:
+            poly:c4d.CPolygon = polys[index]
+            selPointsId.update([poly.a, poly.b, poly.c, poly.d])
+        return selPointsId
 
-def selectedPointTo():
-    objs = doc.GetSelection()
-    if not objs:
-        return
+    @staticmethod
+    def edgesToId(obj) -> set[int]:
+        selEdge: c4d.BaseSelect   = obj.GetEdgeS()
+        polys: list[c4d.CPolygon] = obj.GetAllPolygons()
+
+        selectedEdges = set()
+        for polyIndex, poly in enumerate(polys):
+            edges = [
+                (poly.a, poly.b),
+                (poly.b, poly.c),
+                (poly.c, poly.d if poly.c != poly.d else poly.a),
+                (poly.d if poly.c != poly.d else poly.a, poly.a)
+            ]
+            for edgeIndex, (v1, v2) in enumerate(edges):
+                selectionIndex = 4 * polyIndex + edgeIndex
+                if selEdge.IsSelected(selectionIndex):
+                    selectedEdges.update((v1, v2))
+
+        return selectedEdges
+
+def getSelectedPointsInfo(obj) -> list[int, c4d.Vector]:
+    obj      = obj.GetDeformCache() or obj
+    pointsID = GetSelectedPointID(obj, doc.GetMode()).run()
+    return [[i, obj.GetPoint(i) * obj.GetMg()] for i in pointsID]
+
+def createLoc(pos, name) -> c4d.BaseObject:
+    locator = c4d.BaseObject(c4d.Onull)
+    locator.SetName('{}_LOC_{}'.format(name[0], name[1]))
+    doc.AddUndo(c4d.UNDOTYPE_NEW, locator)
+    doc.InsertObject(locator)
+
+    locator.SetMg(c4d.Matrix(off=pos))
+    locator[c4d.NULLOBJECT_DISPLAY] = 1
+    locator[c4d.NULLOBJECT_ORIENTATION] = 1
+    doc.SetSelection(locator, c4d.SELECTION_ADD)
+
+def main():
+    sel = doc.GetActiveObjects(c4d.GETACTIVEOBJECTFLAGS_CHILDREN)
+    if not sel or not doc.IsEditMode(): return
     doc.StartUndo()
 
-    for obj in objs:
+    for obj in sel:
+        doc.AddUndo(c4d.UNDOTYPE_BITS, obj)
         doc.SetSelection(obj, c4d.SELECTION_SUB)
-        if not isinstance(obj, c4d.PointObject):
+        if not obj.CheckType(c4d.Opoint):
             continue
 
-        cache = obj.GetDeformCache() # get deforme cache
-        obj = obj if cache is None else cache
+        pointsInfo = getSelectedPointsInfo(obj)
+        for info in pointsInfo:
+            createLoc(info[1], (obj.GetName(), info[0]))
 
-        selectedPoints = getSelectedPoints(obj)
-        for point in selectedPoints:
-            pos = obj.GetPoint(point) * obj.GetMg()
-            create(pos, (obj.GetName(), point))
-
-    c4d.CallCommand(12298)
+    doc.SetMode(c4d.Mmodel)
     doc.EndUndo()
     c4d.EventAdd()
+
 if __name__ == '__main__':
-    selectedPointTo()
+    main()
